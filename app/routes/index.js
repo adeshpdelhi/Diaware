ee987e896b9b64a39e779f4d4c11ff6fcac63eec
Adesh Pandey <pandeyadeshdelhi@gmail.com>
2016-07-03 15:35:57 +0530

    cleaned routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,9 @@
+var models  = require('../models');
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+	res.end('WOrking');
 });
 
 module.exports = router;
