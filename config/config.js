0aac1a8f860010fab52f012a58c1df1c7e2e6144
aishraven <https://bitbucket.org/aishraven/>
2016-07-03 16:35:15 +0530

    added config file to git ignore

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'sequelizeUrl' : 'mysql://root:12345@localhost:3306/diaware',
+    'sequelizeUrl' : 'mysql://root:Natural_96@localhost:3306/diaware',
     'facebook' : {
     	clientID: '1213732308644989',
     	clientSecret: 'f481e102f2b18a6849b51e3392a6adba',
