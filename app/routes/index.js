f4a858409a15650013adb716cc699ae778075e11
Adesh Pandey <pandeyadeshdelhi@gmail.com>
2016-07-03 16:11:09 +0530

    fixed route for api

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
 
-router.get('/api', function(req, res) {
+router.get('/', function(req, res) {
 	res.end('WOrking');
 });
 
