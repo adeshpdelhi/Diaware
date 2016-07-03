3efee51b203451c65781fdfc226fc022336b3b18
aishraven <https://bitbucket.org/aishraven/>
2016-07-03 17:22:45 +0530

    frontEnd added to route/index.js

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var debug = require('debug')('express-example');
+var methodOverride = require('method-override');
 
 //var authenticate = require('./authenticate');
 
 var config = require('./config/config');
 
-var routes = require('./app/routes');
+var routes = require('./app/routes/index');
 var models = require('./app/models');
 
 models.sequelize.sync();
@@ -24,22 +25,22 @@ app.all('*', function(req, res, next){
   }
   res.redirect('https://'+ req.hostname+":"+app.get('secPort')+req.url);
 });
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('X-HTTP-Method-Override')); 
 app.use(cookieParser());
 
 app.use(passport.initialize());
 
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/api', routes);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router  = express.Router();
 
 router.get('/', function(req, res) {
-	res.end('WOrking');
+	res.sendfile(path.join(__dirname,'/dist/index.html')); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 module.exports = router;
