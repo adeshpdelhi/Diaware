b43f06574fcd34a2e6a05f46405b83117470d770
Adesh Pandey <pandeyadeshdelhi@gmail.com>
2016-07-04 16:56:33 +0530

    partially working passport

---
+++ app/controllers/signupController.js
@@ -0,0 +1,27 @@
+var bcrypt = require('bcrypt'),
+    users = require('../models').users
+
+
+module.exports.signup = function(req, res) {
+  var username = req.body.username
+  var password = req.body.password
+  
+  var salt = bcrypt.genSaltSync(10)
+  var hashedPassword = bcrypt.hashSync(password, salt)
+  console.log(password+" "+hashedPassword);
+  var newUser = {
+    username: username,
+    salt: salt,
+    hashedPassword: hashedPassword
+  }
+  
+  users.create(newUser).then(function() {
+    res.redirect('/')
+  }).catch(function(error) {
+    req.flash('error', "Please, choose a different username.")
+    res.redirect('/signup')
+  // if(users.findOne({username:username})!=null)
+  //   res.end('successful');
+  // else
+  //   res.end('error');
+}
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,99 +1,35 @@
+var passport = require('passport'),
+    signupController = require('../controllers/signupController');
 var express = require('express');
-var router = express.Router();
-var passport = require('passport');
-var User = require('../models/user');
-var Verify = require('./verify');  
 
-/* GET users listing. */
-router.get('/', Verify.verifyOrdinaryUser, Verify.verifyAdmin, function(req, res, next) {
-  	User.find({}, function(err, user){
-		if(err) throw err;
-		res.json(user);
-	});
-});
+  var router = express.Router();
 
-router.post('/register', function(req, res){
-	User.register(new User({
-		username :req.body.username
-	}),
-	req.body.password, function(err, user){
-		if(err) {
-			return res.status(500).json({err:err});
-		}
-		if(req.body.firstname){
-			user.firstname = req.body.firstname;
-		}
-		if(req.body.lastname){
-			user.lastname = req.body.lastname;
-		}
-		user.save(function(err, user){
-			passport.authenticate('local')(req, res, function(){
-				return res.status(200).json({status:'Registeration Successful!'});
-			});	
-		})
-		
-	});
-});
+  var isAuthenticated = function (req, res, next) {
+    if (req.isAuthenticated())
+      return next();
+    req.flash('error', 'You have to be logged in to access the page.');
+    res.redirect('/');
+  };
+  
+  router.post('/signup', signupController.signup);
 
-router.post('/login', function(req, res, next){
-	passport.authenticate('local',function(err, user, info){
-		if(err){
-			return next(err);
-		}
-		if(!user){
-			return res.status(401).json({
-				err:info
-			});
-		}
-		req.logIn(user, function(err){
-			if(err){
-				return res.status(500).json({
-					err:'Could not login User'
-				});
-			}
-			// console.log('User in users: ',user);
+  router.post('/login', passport.authenticate('local', {
+      successRedirect: '/dashboard',
+      failureRedirect: '/',
+      failureFlash: true 
+  }));
 
-			var token = Verify.getToken(user);
+  router.get('/', function(req, res) {
+    res.end('home');
+  });
 
-			res.status(200).json({
-				status:'Login Successful',
-				success:true,
-				token:token
-			});
-		});
-	})(req, res, next);
-});
+  router.get('/dashboard', isAuthenticated, function(req, res) {
+    res.end('dashboard');
+  });
 
-router.get('/logout', function(req, res){
-	req.logout();
-	res.status(200).json({
-		status:'Bye!'
-	});
-});
+  router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
 
-
-router.get('/facebook', passport.authenticate('facebook'), function(req, res){});
-router.get('/facebook/callback', function(req, res, next){
-	passport.authenticate('facebook', function(err, user, info){
-		if(err){
-			return next(err);
-		}
-		if(!user){
-			return res.status(401).json({ err:info});
-		}
-		req.logIn(user, function(err){
-			if(err){
-				return res.status(500).json({
-					err:'Could not login user'
-				});
-			}
-			var token = Verify.getToken(user);
-			res.status(200).json({
-				status:'Login Successful!',
-				success:true,
-				token: token
-			});
-		});
-	})(req,res,next);
-});
 module.exports = router;
