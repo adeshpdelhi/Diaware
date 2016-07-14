a0c2e8529727f39eae4217d0277ee760c3d311d8
rishabh <rishabh999gupta@gmail.com>
2016-07-14 18:17:21 +0530

    clinical plan view done

--- public/scripts/controllers/dialysis_care_plan/viewDialysisCarePlanController.js
+++ public/scripts/controllers/dialysis_care_plan/viewDialysisCarePlanController.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('App')
-.controller('ViewDialysisCarePlanController',['$scope','patientFactory', 'authorize','choosePatientFactory', function($scope,patientFactory,choosePatientFactory,authorize){
+.controller('ViewDialysisCarePlanController',['$scope','patientFactory','choosePatientFactory', 'authorize', function($scope,patientFactory,choosePatientFactory,authorize){
 	
 	patientFactory.getPatients(authorize.getCentre()).get({id:choosePatientFactory.getChosenPatient().id}).$promise.then(function(response){
         	$scope.patient = response;
