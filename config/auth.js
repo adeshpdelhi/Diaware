201e21da1191b2bf1a28b583c5ee31bb689dc7d8
Adesh Pandey <pandeyadeshdelhi@gmail.com>
2016-07-14 16:50:48 +0530

    minor fix when not entering username/password

--- config/auth.js
+++ config/auth.js
@@ -46,8 +46,9 @@ exports.logout = function(req, res){
 	}
 }
 exports.register = function(req, res){
+	if(req.body.username!=null && req.body.password!=null)
 	console.log("registering for "+req.body.username+" "+crypto.createHash('md5').update(req.body.password).digest("hex"));
-	if(req.body.username !=null && req.body.password !=null && req.body.centres !=null && req.body.admin !=null && req.body.incharge !=null && req.body.manager !=null && req.body.clinical !=null)
+	if(req.body.username !=null && req.body.password !=null && req.body.centres !=null && req.body.admin !=null && req.body.incharge !=null && req.body.manager !=null && req.body.clinical !=null &&req.body.centres!='')
 	{
 		users.findOne({where: {username: req.body.username}}).then(function(user){
 			if(user !=null){
@@ -56,6 +57,7 @@ exports.register = function(req, res){
 			}
 			else
 			{
+				req.body.centres=req.body.centres.replace(/\s/g, '');
 				users.create({username: req.body.username, hashedPassword: crypto.createHash('md5').update(req.body.password).digest("hex"), centres: req.body.centres, admin: req.body.admin, incharge: req.body.incharge, manager: req.body.manager, clinical: req.body.clinical});
 				res.end('Successfully added: '+req.body.username);
 			}
