858a3203ae14635625b5b4392e89243653b645e2
Adesh Pandey <pandeyadeshdelhi@gmail.com>
2016-07-16 01:05:42 +0530

    admin db updation completely working

--- public/scripts/controllers/admin/dataBaseController.js
+++ public/scripts/controllers/admin/dataBaseController.js
@@ -6,6 +6,10 @@ angular.module('App')
 	$scope.panel={};
 	$scope.dialyzateType={};
 	$scope.transactionType={};
+	$scope.dialysisType={};
+	$scope.consumableType={};
+	$scope.procedureType={};
+	$scope.pharmacyType={};
 	$scope.shouldShowForm = false;
 	$scope.displayList = function(){
 		console.log($scope.table);
@@ -15,6 +19,14 @@ angular.module('App')
 			$scope.showDialyzateTypes();
 		if($scope.table == 'TransactionTypes')
 			$scope.showTransactionTypes();
+		if($scope.table == 'DialysisTypes')
+			$scope.showDialysisTypes();
+		if($scope.table == 'ConsumableTypes')
+			$scope.showConsumableTypes();
+		if($scope.table == 'ProcedureTypes')
+			$scope.showProcedureTypes();
+		if($scope.table == 'PharmacyTypes')
+			$scope.showPharmacyTypes();
 	};
 	$scope.addPanel = function(){
 		console.log($scope.panel);
@@ -113,6 +125,109 @@ angular.module('App')
 	$scope.deleteTransactionType = function(transactionType){
 		backendFactory.getTransactionTypes().delete({transactionType:transactionType});
 		$scope.showTransactionTypes();
+	}
+
+
+	$scope.addDialysisType = function(){
+		console.log($scope.dialysisType);
+		backendFactory.getDialysisTypes().save($scope.dialysisType).$promise.then(function(response){
+						alert('DialysisType saved');
+						$scope.showDialysisTypes();
+						$scope.showForm=false;
+				},function(response){
+					alert('dialysisType save failed!');
+				});
+	};
+
+	$scope.showDialysisTypes = function(){
+		console.log('showing dialysisType');
+		backendFactory.getDialysisTypes().query().$promise.then(function(response){
+					$scope.dialysisTypes=response;
+				},function(response){
+					alert('dialysisTypes retrieval failed!');
+				});
+	};
+
+	$scope.deleteDialysisType = function(dialysisType){
+		backendFactory.getDialysisTypes().delete({dialysisType:dialysisType});
+		$scope.showDialysisTypes();
+	}
+
+
+	$scope.addConsumableType = function(){
+		console.log($scope.consumableType);
+		backendFactory.getConsumableTypes().save($scope.consumableType).$promise.then(function(response){
+						alert('ConsumableType saved');
+						$scope.showConsumableTypes();
+						$scope.showForm=false;
+				},function(response){
+					alert('consumableType save failed!');
+				});
+	};
+
+	$scope.showConsumableTypes = function(){
+		console.log('showing consumableType');
+		backendFactory.getConsumableTypes().query().$promise.then(function(response){
+					$scope.consumableTypes=response;
+				},function(response){
+					alert('consumableTypes retrieval failed!');
+				});
+	};
+
+	$scope.deleteConsumableType = function(consumableType){
+		backendFactory.getConsumableTypes().delete({consumableType:consumableType});
+		$scope.showConsumableTypes();
+	}
+
+
+	$scope.addProcedureType = function(){
+		console.log($scope.procedureType);
+		backendFactory.getProcedureTypes().save($scope.procedureType).$promise.then(function(response){
+						alert('ProcedureType saved');
+						$scope.showProcedureTypes();
+						$scope.showForm=false;
+				},function(response){
+					alert('procedureType save failed!');
+				});
+	};
+
+	$scope.showProcedureTypes = function(){
+		console.log('showing procedureType');
+		backendFactory.getProcedureTypes().query().$promise.then(function(response){
+					$scope.procedureTypes=response;
+				},function(response){
+					alert('procedureTypes retrieval failed!');
+				});
+	};
+
+	$scope.deleteProcedureType = function(procedureType){
+		backendFactory.getProcedureTypes().delete({procedureType:procedureType});
+		$scope.showProcedureTypes();
+	}
+
+	$scope.addPharmacyType = function(){
+		console.log($scope.pharmacyType);
+		backendFactory.getPharmacyTypes().save($scope.pharmacyType).$promise.then(function(response){
+						alert('PharmacyType saved');
+						$scope.showPharmacyTypes();
+						$scope.showForm=false;
+				},function(response){
+					alert('pharmacyType save failed!');
+				});
+	};
+
+	$scope.showPharmacyTypes = function(){
+		console.log('showing pharmacyType');
+		backendFactory.getPharmacyTypes().query().$promise.then(function(response){
+					$scope.pharmacyTypes=response;
+				},function(response){
+					alert('pharmacyTypes retrieval failed!');
+				});
+	};
+
+	$scope.deletePharmacyType = function(pharmacyType){
+		backendFactory.getPharmacyTypes().delete({pharmacyType:pharmacyType});
+		$scope.showPharmacyTypes();
 	}
 
 }]);
