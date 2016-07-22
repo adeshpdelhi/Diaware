0c8aa6e0ddbc10e4ac17c835a25c858a1441f8ce
Adesh Pandey <pandeyadeshdelhi@gmail.com>
2016-07-22 14:10:05 +0530

    script for inverntroy db

--- inv mod script.sql
+++ inv mod script.sql
@@ -1,215 +1,88 @@
-create table indents(
-	centreId varchar NOT NULL,
-	indentId int primary key AUTOINCREMENT,
-	itemNumber int,
-	itemName varchar -- to simulate array
-	usageType varchar
-	brandName varchar
-	quantityRequired int
-	availabilityQuantity int, --retrieve from stock
-	quantityType varchar
-	stockOrderTo varchar, --could be reference to vendor table + corporate office
-	status varchar -- status could be: saved, sent, approved, rejected 
+drop database if exists temp;
+create database temp;
+use temp;
+
+create table indents( -- if status sent for approval in admin then here status is sent for approval as well
+	centreId varchar(60) NOT NULL,
+	indentId int primary key AUTO_INCREMENT,
+	requestDate date,
+	requiredByDate date,
+	stockOrderTo varchar(60), -- could be reference to vendor table + corporate office
+	status varchar(60), -- status could be: saved, sent, approved, rejected , fulfilled
+	itemsRaised varchar(60) references indentsItems(indentItemsId),
+	itemsApproved varchar(60) references indentsItems(indentItemsId),
+	itemsReceived varchar(60) references indentsItems(indentItemsId)
 );
 
-create table fulfilledIndents(
-	centreId varchar NOT NULL,
-	indentId int primary key AUTOINCREMENT,
+create table indentsItems(
+	indentItemsId varchar(60), -- indentItemsId is centre+current date time
 	itemNumber int,
-	itemName varchar
-	usageType varchar
-	brandName varchar
-	quantityRequired int
-	availabilityQuantity int, --retrieve from stock
-	quantityType varchar
-	stockOrderTo varchar, --could be reference to vendor table + corporate office
-	status varchar -- status could be: saved, sent, approved, rejected 
+	itemName varchar(60), -- to simulate array
+	usageType varchar(60),
+	brandName varchar(60),
+	quantityRequired int,
+	availabilityQuantity int, -- retrieve from stock
+	quantityType varchar(60),
+	primary key(indentItemsId,itemNumber)
 );
 
 create table stock(
-	centreId varchar NOT NULL primary key,
-	itemId int primary key,
-	itemName varchar
-	usageType varchar
-	brandName varchar
-	availabilityQuantity int
-	quantityType varchar
+	centreId varchar(60) NOT NULL,
+	itemId bigint AUTO_INCREMENT primary key,
+	itemName varchar(60),
+	usageType varchar(60),
+	brandName varchar(60),
+	availabilityQuantity int,
+	quantityType varchar(60)
 );
 
 create table stockIssued(
-	centreId varchar NOT NULL primary key,
-	stockIssuedTo varchar
-	estimatedTreatmentsNextDay int
-	stockIssueDate date
-	stockTakerName varchar
-
-	itemNumber int, -- to simulate array		   ~~~~~~~~~\
-	itemId int --											 \ 
-	itemName varchar --										  \
-	usageType varchar --									   ---------- denotes a single item entry
-	brandName varchar --									  /
-	quantity int --											 /
-	quantityType varchar --						   _________/
-
-
+	centreId varchar(60) NOT NULL,
+	stockIssuedId bigint primary key,
+	stockIssuedTo varchar(60),
+	estimatedTreatmentsNextDay int,
+	stockIssueDate date,
+	stockTakerName varchar(60),
 	nextExpectedStockIssueDate date
 );
 
+create table stockIssuedItems(
+	stockIssuedId bigint references stockIssued(stockIssuedId),
+	itemNumber int, -- to simulate array		   ~~~~~~~~~\
+	itemId int, --											 \ 
+	itemName varchar(60), --										  \
+	usageType varchar(60), --									   ---------- denotes a single item entry
+	brandName varchar(60), --									  /
+	quantity int, --											 /
+	quantityType varchar(60) --						   _________/
+);
 
 create table consumption(
-	treatementId primary key references monitoringChartPreBasic(preBasicId),
-	treatementType varchar
+	centreId varchar(60) NOT NULL,
+	treatementType varchar(60),
+	treatementId varchar(60) primary key,
+	itemName varchar(60),
+	brand varchar(60),
+	type varchar(60),
+	quanityType varchar(60),
+	quantity int
 );
 
-create table consumptionDialysis(
-	treatementId primary key references consumption(treatementId),
-	dialyzerBrand,
-	bloodTubing Brand,
-	fistulaNeedleBrand,
-	IvSetBrand,
-	sterileGlovesBrand,
-	normalSaline1000mlBrand,
-	normalSaline500mlBrand,
-	syringe10mlBrand,
-	syringe20mlBrand,
-	syringe1mlBrand,
-	heparinBrand,
-	dialysisKitBrand,
-	gauzePiecesBrand,
-	transducerProtectorBrand,
-	partABrand,
-	partBBrand,
-	unsterileGlovesBrand,
-	
-	dialyzerType,
-	bloodTubing Type,
-	fistulaNeedleType,
-	IvSetType,
-	sterileGlovesType,
-	normalSaline1000mlType,
-	normalSaline500mlType,
-	syringe10mlType,
-	syringe20mlType,
-	syringe1mlType,
-	heparinType,
-	dialysisKitType,
-	gauzePiecesType,
-	transducerProtectorType,
-	partAType,
-	partBType,
-	unsterileGlovesType,
-	
-	dialyzerQuantityType,
-	bloodTubing QuantityType,
-	fistulaNeedleQuantityType,
-	IvSetQuantityType,
-	sterileGlovesQuantityType,
-	normalSaline1000mlQuantityType,
-	normalSaline500mlQuantityType,
-	syringe10mlQuantityType,
-	syringe20mlQuantityType,
-	syringe1mlQuantityType,
-	heparinQuantityType,
-	dialysisKitQuantityType,
-	gauzePiecesQuantityType,
-	transducerProtectorQuantityType,
-	partAQuantityType,
-	partBQuantityType,
-	unsterileGlovesQuantityType,
-	
-	dialyzerQuantity,
-	bloodTubing Quantity,
-	fistulaNeedleQuantity,
-	IvSetQuantity,
-	sterileGlovesQuantity,
-	normalSaline1000mlQuantity,
-	normalSaline500mlQuantity,
-	syringe10mlQuantity,
-	syringe20mlQuantity,
-	syringe1mlQuantity,
-	heparinQuantity,
-	dialysisKitQuantity,
-	gauzePiecesQuantity,
-	transducerProtectorQuantity,
-	partAQuantity,
-	partBQuantity,
-	unsterileGlovesQuantity
+create table dialysisItems(
+	itemName varchar(60) primary key
 );
 
-create table consumptionCatheterization(
-	treatementId primary key references consumption(treatementId),
-
-
-	doubleLumenCatheterBrand,
-	singleLumenCatheterBrand,
-	guideWireBrand,
-	introducerNeedle Brand,
-	sterileGlovesBrand,
-	syringe5mlBrand,
-	syringe10mlBrand,
-	syringe20mlBrand,
-	syringe1mlBrand,
-	heparinBrand,
-	dialysisKitBrand,
-	gauzePiecesBrand,
-	unsterileGlovesBrand,
-	disposableAppronBrand,
-
-	doubleLumenCatheterType,
-	singleLumenCatheterType,
-	guideWireType,
-	introducerNeedle Type,
-	sterileGlovesType,
-	syringe5mlType,
-	syringe10mlType,
-	syringe20mlType,
-	syringe1mlType,
-	heparinType,
-	dialysisKitType,
-	gauzePiecesType,
-	unsterileGlovesType,
-	disposableAppronType,
-
-	doubleLumenCatheterQuantityType,
-	singleLumenCatheterQuantityType,
-	guideWireQuantityType,
-	introducerNeedle QuantityType,
-	sterileGlovesQuantityType,
-	syringe5mlQuantityType,
-	syringe10mlQuantityType,
-	syringe20mlQuantityType,
-	syringe1mlQuantityType,
-	heparinQuantityType,
-	dialysisKitQuantityType,
-	gauzePiecesQuantityType,
-	unsterileGlovesQuantityType,
-	disposableAppronQuantityType,
-
-
-	doubleLumenCatheterQuantity,
-	singleLumenCatheterQuantity,
-	guideWireQuantity,
-	introducerNeedleQuantity,
-	sterileGlovesQuantity,
-	syringe5mlQuantity,
-	syringe10mlQuantity,
-	syringe20mlQuantity,
-	syringe1mlQuantity,
-	heparinQuantity,
-	dialysisKitQuantity,
-	gauzePiecesQuantity,
-	unsterileGlovesQuantity,
-	disposableAppronQuantity,
-
+create table catheterizationItems(
+	itemName varchar(60) primary key
 );
 
 create table vendor(
-	vendorId,
-	vendorName,
-	vendorAddress,
-	vendorTINNumber,
-	vendorContactPerson,
-	vendorContactPersonNumver,
-	vendorIntroducedBy,
-	vendorintroducedByName
+	vendorId int primary key AUTO_INCREMENT,
+	vendorName varchar(60),
+	vendorAddress varchar(60),
+	vendorTINNumber varchar(60),
+	vendorContactPerson varchar(60),
+	vendorContactPersonNumver varchar(60),
+	vendorIntroducedBy varchar(60),
+	vendorintroducedByName varchar(60)
 );
