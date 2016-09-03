e5c503f781725373a18e87ead3ce3d755eb2d4d2
Adesh Pandey <pandeyadeshdelhi@gmail.com>
2016-09-03 17:18:30 +0530

    improved matrix

--- public/scripts/services/appointmentFactory.js
+++ public/scripts/services/appointmentFactory.js
@@ -36,6 +36,33 @@ angular.module('App')
           
           });
         };
+        this.getAppointments = function(centreId){
+          return $resource(baseURL+":centreId/appointments/:appointmentId",{centreId:centreId},  {
+            'update':{method:'PUT' },
+          query: {
+              method: 'get', 
+              isArray: true,
+              interceptor: {
+                response: function(response) {      
+                var result = response.resource;        
+                result.$status = response.status;
+                return result;
+              }
+            }
+          },
+          get:{
+                method:'GET',
+                interceptor: {
+                  response: function(response) {      
+                  var result = response.resource;        
+                  result.$status = response.status;
+                  return result;
+                }
+              }
+            }
+          
+          });
+        };
         this.getPastAppointments = function(centreId){
           return $resource(baseURL+":centreId/pastAppointments/:appointmentId",{centreId:centreId},  {
             'update':{method:'PUT' },
