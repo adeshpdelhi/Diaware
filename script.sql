6f51fef921807c70121f4637bdc5516fa3c7bcf9
Adesh Pandey <pandeyadeshdelhi@gmail.com>
2016-09-04 20:32:08 +0530

    billindDue in matrix shown in yellow

--- script.sql
+++ script.sql
@@ -5,7 +5,7 @@ insert into centres (id, name,location,maxPatients,accessLinesAvailable, patient
 insert into patientDetails (id,name,contact,lastModifiedBy,centreId,createdAt,updatedAt,type) values("JP1-2016-1","adesh","987654321","aish", "JP1",'2016-07-13 10:48:56','2016-07-13 10:48:56','Negative');
 -- insert into bills(transactionId,status,amount,lastModifiedBy,patientId,createdAt,updatedAt) values(1,"Paid",26.89,"aish","JP1-2016-1",'2016-07-13 10:48:56','2016-07-13 10:48:56');
 -- insert into appointments(appointmentId, patientId, centreId,billingDone, date) values (DEFAULT,"JP1-2016-1","JP1",true, "2016-07-14");
--- insert into appointments (centreId,date,dayOfTheWeek,appointmentType,tmtMachine,patientId,shiftNumber,oneTimeAppointment,createdAt,updatedAt) values('JP1','2016-09-2','Friday','OPD','NegativeMachine','JP1-2016-1',2,false,'2016-07-13 10:48:56','2016-07-13 10:48:56');
+insert into appointments (centreId,date,dayOfTheWeek,appointmentType,tmtMachine,patientId,shiftNumber,oneTimeAppointment,createdAt,updatedAt) values('JP1','2016-09-2','Friday','OPD','NegativeMachine','JP1-2016-1',2,false,'2016-07-13 10:48:56','2016-07-13 10:48:56');
 
 insert into centres( id, name,location,maxPatients,accessLinesAvailable, patientCount,noOfShiftsPerDay,OPDTotalNegativeMachines,typesOfMachinesAvailable,createdAt,updatedAt) values("CH","chandigarh","chandigarh",50,"femoral,fistoral,IJ",1,4,15,"NegativeMachines",'2016-07-13 10:48:56','2016-07-13 10:48:56');
 insert into panels (id, name, details,createdAt,updatedAt) values(1,"cghs","govt",'2016-07-13 10:48:56','2016-07-13 10:48:56');
